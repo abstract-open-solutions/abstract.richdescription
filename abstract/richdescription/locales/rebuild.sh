6f754f144bc04351560ba02a4dd8a0d97bb5d2b5
Biagio Grimaldi <biagio.grimaldi@abstract.it>
2012-05-28 14:44:00 +0200

    minor fix

--- abstract/richdescription/locales/rebuild.sh
+++ abstract/richdescription/locales/rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PRODUCT=isfol.content
+PRODUCT=abstract.richdescription
 
 i18ndude rebuild-pot --pot ../locales/${PRODUCT}.pot --create $PRODUCT ../
 i18ndude sync --pot ../locales/${PRODUCT}.pot ../locales/*/LC_MESSAGES/${PRODUCT}.po
