45522059b5b15cfbb2b87a8a9f039b5cefbf0b4f
Simone Orsi <simahawk@gmail.com>
2013-03-12 10:15:39 +0100

    move fix to view

---
+++ abstract/richdescription/browser/fix.py
@@ -0,0 +1,34 @@
+import transaction
+
+from Products.Five.browser import BrowserView
+
+
+class FixEmpty(BrowserView):
+
+    def __call__(self):
+        portal = self.context
+
+        catalog = portal.portal_catalog
+        ard_prefs = portal.portal_properties.richdescription_properties
+
+        portal_type = list(ard_prefs.allowed_types)
+
+        brains = catalog(portal_type=portal_type)
+        for i, brain in enumerate(brains):
+            print i
+            obj = brain.getObject()
+            field = obj.getField('rich_description')
+            if field:
+                rich_description = field.get(obj)
+                if not rich_description:
+                    # let's clean up description
+                    obj.setDescription("")
+                    obj.reindexObject(idxs=['Description'])
+                    print 'FIXED', brain.getPath()
+            else:
+                print 'NO FIELD', brain.getPath()
+            if i % 500:
+                transaction.savepoint(optimistic=1)
+
+        transaction.commit()
+        return 'DONE!'
